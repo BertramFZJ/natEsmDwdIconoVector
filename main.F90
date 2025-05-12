74e19820c751f2c04cce5eedb330551d791da755
BertramFZJ <s.sukov.jsc@gmail.com>
2025-05-12 15:41:19 +0200

    Update main.F90

--- main.F90
+++ main.F90
@@ -4,68 +4,48 @@ PROGRAM MAIN
 
     IMPLICIT NONE
 
-    DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:,:) :: matrixARG
-    DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:,:) :: matrixSIN2
-    DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:,:) :: matrixCOS2
-    DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:,:) :: matrixRES
+    DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:) :: vectorArg
+    DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:) :: vectorSin
+
+    INTEGER :: nList
+    INTEGER, ALLOCATABLE, DIMENSION(:) :: indexList    
     
-    INTEGER, PARAMETER :: N = 10240
+    INTEGER, PARAMETER :: N = 10240 * 10240
     
-    INTEGER :: I, J, ID
+    INTEGER :: I
 
     DOUBLE PRECISION :: startTimer, finishTimer
     
     WRITE(*,*) 'PROGRAM START'
 
-    ALLOCATE( matrixARG(1:N,1:N), matrixSIN2(1:N,1:N), matrixCOS2(1:N,1:N), matrixRES(1:N,1:N) )
+    ALLOCATE( vectorArg(1:N), vectorSin(1:N), indexList(1:N) )
 
-    startTimer = omp_get_wtime()
+    DO I = 1, N
+        vectorArg(I) = I * (6.28 / N)
+        vectorSin(I) = 0.0;
+    END DO
+    
+    nList = 0
+    indexList = -1
 
-    DO J = 1, N
-        DO I = 1, N
-            ID = I + (J - 1) * N - 1
-            matrixARG(I,J) = ID * 0.0123
-        END DO
-    ENDDO
+    startTimer  = omp_get_wtime()
 
-    !$OMP PARALLEL DO PRIVATE(I) NUM_THREADS(4)
-    DO J = 1, N
-        !$OMP SIMD
-        DO I = 1, N
-            matrixSIN2(I,J) = SIN(matrixARG(I,J))
-            matrixSIN2(I,J) = matrixSIN2(I,J) * matrixSIN2(I,J)
-            matrixCOS2(I,J) = COS(matrixARG(I,J))
-            matrixCOS2(I,J) = matrixCOS2(I,J) * matrixCOS2(I,J)
-        END DO
-    ENDDO
-    !$OMP END PARALLEL DO
-
-    DO J = 1, N
-        !$OMP SIMD
-        DO I = 1, N
-            CALL subSUM(matrixSIN2(I,J), matrixCOS2(I,J), matrixRES(I,J))
-        END DO
-    ENDDO
-
-    ! CALL subSUM(matrixSIN2(:,:), matrixCOS2(:,:), matrixRES(:,:))
+    DO I = 1, N
+        vectorSin(I) = SIN(vectorArg(I))
+        
+        IF(vectorSin(I) < 0.0) THEN
+            nList = nList + 1
+            indexList(nList) = I
+        END IF
+    END DO
 
     finishTimer = omp_get_wtime()
-    WRITE(*,*) "TIME ", finishTimer - startTimer
+    
+    WRITE(*,*) 'TIME: ', finishTimer - startTimer
+    WRITE(*,*) 'LIST LENGTH: ', nList
+    WRITE(*,*) 'INDEXES: ', MINVAL(indexList(1:nList)), '<> ', &
+                            MAXVAL(indexList(1:nList))
 
-    WRITE(*,*) 'PROGRAM FINISH'
-
-    CONTAINS
-
-    PURE SUBROUTINE subSUM(a, b, c)
-
-        IMPLICIT NONE
-
-        DOUBLE PRECISION, INTENT(in)    :: a
-        DOUBLE PRECISION, INTENT(in)    :: b
-        DOUBLE PRECISION, INTENT(inout) :: c
-
-        c = a + b    
-
-    END SUBROUTINE subSUM
+     WRITE(*,*) 'PROGRAM FINISH'    
 
 END PROGRAM MAIN
