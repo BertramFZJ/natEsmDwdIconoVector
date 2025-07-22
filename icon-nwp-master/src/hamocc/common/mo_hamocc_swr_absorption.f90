3ca86f0a27b5450cd4789ecb5707c032634aa5e9
BertramFZJ <s.sukov.jsc@gmail.com>
2025-07-22 17:10:00 +0200

    Vectorization of subroutine mo_hamocc_swr_absorption::swr_absorption

--- icon-nwp-master/src/hamocc/common/mo_hamocc_swr_absorption.f90
+++ icon-nwp-master/src/hamocc/common/mo_hamocc_swr_absorption.f90
@@ -28,6 +28,8 @@ MODULE mo_hamocc_swr_absorption
 
 CONTAINS
 
+#if 0
+
 SUBROUTINE swr_absorption(local_bgc_mem, start_idx,end_idx, klevs, pfswr, psicomo, dzw, lacc)
 
     TYPE(t_bgc_memory), POINTER :: local_bgc_mem
@@ -102,4 +104,96 @@ SUBROUTINE swr_absorption(local_bgc_mem, start_idx,end_idx, klevs, pfswr, psicom
 
 
 END SUBROUTINE swr_absorption
+
+#else
+
+SUBROUTINE swr_absorption(local_bgc_mem, start_idx,end_idx, klevs, pfswr, psicomo, dzw, lacc)
+
+    TYPE(t_bgc_memory), POINTER :: local_bgc_mem
+    INTEGER, INTENT(in):: start_idx
+    INTEGER, INTENT(IN):: end_idx
+    INTEGER :: klevs(bgc_nproma)
+    REAL(wp), INTENT(in):: pfswr(bgc_nproma)
+    REAL(wp), INTENT(in):: psicomo(bgc_nproma)
+    REAL(wp), INTENT(in):: dzw(bgc_nproma,bgc_zlevs)
+    LOGICAL, INTENT(IN), OPTIONAL :: lacc
+
+    !! Analogue to Zielinski et al., Deep-Sea Research II 49 (2002), 3529-3542
+
+    REAL(wp), PARAMETER :: redfrac=0.4_wp !< red fraction of the spectral domain (> 580nm)
+
+    REAL(wp), PARAMETER :: c_to_chl=12.0_wp/60.0_wp   !< ration Carbon to Chlorophyll
+    REAL(wp), PARAMETER :: r_car=122.0_wp   !< Redfield ratio
+    REAL(wp), PARAMETER :: pho_to_chl=r_car*c_to_chl*1.e6_wp !< 1 kmolP = (122*12/60)*10^6 mg[Chlorophyll]
+
+    REAL(wp), PARAMETER :: atten_r=0.35_wp !< attenuation of red light [m-1]
+    REAL(wp), PARAMETER :: atten_w=0.03_wp !< attenuation of blue/green light
+                                           !! in clear water between 400nm and 580nm [m-1]
+    REAL(wp), PARAMETER :: atten_c=0.04_wp !< attenuation of blue/green light
+                                           !! by chlorophyll [m-1]
+
+    REAL(wp) :: swr_r(start_idx:end_idx)
+    REAL(wp) :: swr_b(start_idx:end_idx)
+    REAL(wp) :: rcyano
+
+    INTEGER :: k, j
+
+    INTEGER :: max_klevs
+
+    ! if prognostic cyanobacteria are calculated
+    ! use them in absorption (rcyano=1)
+    rcyano = merge(1._wp, 0._wp, l_cyadyn)
+
+    max_klevs = MAXVAL(klevs(start_idx:end_idx))
+
+    !NEC$ nomove
+    DO j = start_idx, end_idx
+        local_bgc_mem%strahl(j) = pfswr(j) * (1._wp - psicomo(j))
+        local_bgc_mem%swr_frac(j,1) = 1.0_wp
+
+        IF(klevs(j) > 0) THEN
+            swr_r(j) = redfrac
+            swr_b(j) = (1._wp-redfrac)
+        END IF
+    END DO
+
+    DO k = 2, max_klevs
+        !NEC$ nomove
+        DO j = start_idx, end_idx
+
+            IF(k <= klevs(j)) THEN
+                swr_r(j) = swr_r(j) * EXP(-dzw(j,k-1) * &
+                       &   atten_r)
+
+                swr_b(j) = swr_b(j) * EXP(-dzw(j,k-1) * &
+                       &  (atten_w + atten_c * pho_to_chl * MAX(0.0_wp,(local_bgc_mem%bgctra(j,k-1,iphy) + rcyano * local_bgc_mem%bgctra(j,k-1,icya)))))
+
+                local_bgc_mem%swr_frac(j,k) = swr_r(j) + swr_b(j)
+            END IF
+
+        END DO
+    END DO
+
+    DO k = 1, max_klevs - 1
+        !NEC$ nomove
+        DO j = start_idx, end_idx
+
+            IF(k <= klevs(j) - 1) THEN
+                local_bgc_mem%meanswr(j,k) = (local_bgc_mem%swr_frac(j,k) + local_bgc_mem%swr_frac(j,k+1))/2._wp
+            END IF
+
+        END DO
+    END DO
+
+    !NEC$ nomove
+    DO j = start_idx, end_idx
+        IF(klevs(j) > 0) THEN
+            local_bgc_mem%meanswr(j,klevs(j)) = local_bgc_mem%swr_frac(j,klevs(j))
+        END IF
+    END DO
+
+END SUBROUTINE swr_absorption
+
+#endif
+
 END MODULE
